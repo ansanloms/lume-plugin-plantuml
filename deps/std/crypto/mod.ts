911c67d3b849adc36ce4cadea73934752ea99dbf
ansanloms <ansanloms@users.noreply.github.com>
2023-08-07 16:06:12 +0900

    upgrade

--- deps/std/crypto/mod.ts
+++ deps/std/crypto/mod.ts
@@ -1,2 +1,2 @@
-import * as mod from "https://deno.land/std@0.185.0/crypto/mod.ts";
+import * as mod from "https://deno.land/std@0.197.0/crypto/mod.ts";
 export const { crypto } = mod;
--- deps/std/path/mod.ts
+++ deps/std/path/mod.ts
@@ -1,2 +1,2 @@
-import * as mod from "https://deno.land/std@0.185.0/path/mod.ts";
+import * as mod from "https://deno.land/std@0.197.0/path/mod.ts";
 export const { dirname, join } = mod;
